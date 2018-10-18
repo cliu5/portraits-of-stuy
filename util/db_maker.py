357d56a2c9081c0be8658b93426f963330f2b7eb
raywu6 <rwu6@stuy.edu>
2018-10-18 13:00:34 -0400

    add contributions table to db

--- util/db_maker.py
+++ util/db_maker.py
@@ -1,17 +1,24 @@
 import sqlite3   #enable control of an sqlite database
-import csv       #facilitates CSV I/O
 
 def createUserTable(cur):
-    command = "CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT, password TEXT)"
+    command = "CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL)"
     cur.execute(command)
+    
 def createStoriesTable(cur):
-    command = "CREATE TABLE stories(id INTEGER PRIMARY KEY, title TEXT, body TEXT, latestAddition TEXT)"
+    command = "CREATE TABLE stories(id INTEGER PRIMARY KEY, title TEXT NOT NULL, body TEXT NOT NULL, latestAddition TEXT NOT NULL)"
     cur.execute(command)
+    
+def createContributionsTable(cur):
+    command = "CREATE TABLE contributions (id INTEGER PRIMARY KEY, user_id INTEGER NOT NULL FOREIGN KEY, story_id INTEGER NOT NULL FOREIGN KEY)"
+    cur.execute(command)
+    
 def main():
     DB_FILE= "foo.db"
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
     createUserTable(c)
     createStoriesTable(c)
+    createContributionsTable(c)
     db.close()  #close database
+
 main()
