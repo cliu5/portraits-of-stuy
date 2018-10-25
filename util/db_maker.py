2e35a629f56b07ca0e56a9d16be8dc6ff26f75a6
tina <tinaw425@gmail.com>
2018-10-25 12:01:04 -0400

    updated db

--- util/db_maker.py
+++ util/db_maker.py
@@ -1,18 +1,23 @@
 import sqlite3   #enable control of an sqlite database
 
-def createUserTable(cur):
+def create_user_table(cur):
     ''' This function creates Users table in database with column names id, username, and password.'''
     command = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL)" #build SQL stmt, save as string
     cur.execute(command) #run SQL statement
 
-def createStoriesTable(cur):
+def create_stories_table(cur):
     ''' This function creates Stories table in database with column names id, title, body, and latestAddition.'''
-    command = "CREATE TABLE IF NOT EXISTS stories (id INTEGER PRIMARY KEY, title TEXT NOT NULL, body TEXT NOT NULL, latestAddition TEXT NOT NULL)" #build SQL stmt, save as string
+    command = "CREATE TABLE IF NOT EXISTS stories (id INTEGER PRIMARY KEY, title TEXT NOT NULL, body TEXT NOT NULL, latestAddition TEXT NOT NULL, total_rating INTEGER)" #build SQL stmt, save as string
     cur.execute(command) #run SQL statement
 
-def createContributionsTable(cur):
+def create_contributions_table(cur):
     ''' This function creates Contributions table in database with column names id, user_id, and story_id.'''
     command = "CREATE TABLE IF NOT EXISTS contributions (id INTEGER PRIMARY KEY, user_id INTEGER NOT NULL, story_id INTEGER NOT NULL)" #build SQL stmt, save as string
+    cur.execute(command) #run SQL statement
+
+def create_ratings_table(cur):
+    ''' This function creates Ratings table in database with column names id, user_id, story_id, and user_rating.'''
+    command = "CREATE TABLE IF NOT EXISTS ratings (id INTEGER PRIMARY KEY, user_id INTEGER NOT NULL, story_id INTEGER NOT NULL, user_rating INTEGER)" #build SQL stmt, save as string
     cur.execute(command) #run SQL statement
 
 def createDatabase():
@@ -20,7 +25,7 @@ def createDatabase():
     DB_FILE= "foo.db"
     db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
     c = db.cursor() #facilitate db ops
-    createUserTable(c)
-    createStoriesTable(c)
-    createContributionsTable(c)
+    create_user_table(c)
+    create_stories_table(c)
+    create_contributions_table(c)
     db.close() #close database
